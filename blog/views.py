c4c397d8bbcc38eb37e71d34f5fcd2239d1f6319
Usman Pervez <152008530+Usman6768@users.noreply.github.com>
2024-07-12 16:33:20 +0500

    Added Styling

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
+
+posts = [
+    {
+        'author' : 'Usman',
+        'title' : 'Blog 1',
+        'content': 'Hello this is my blog',
+        'date_posted':'12-07-2024'
+    },
+    {
+        'author' : 'Alex',
+        'title' : 'Python tips',
+        'content': 'Hello this is my pykit',
+        'date_posted':'18-07-2018'
+    }
+]
+
+
 def home(request):
-    return HttpResponse("HEllo testing")
+    context = {
+        'posts':posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('About Section')
+    return render(request, 'blog/about.html')
